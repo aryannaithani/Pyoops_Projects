86cb89293c8a479525bd4f5a29bd2e0261d88496
aryannaithani <aryannaithani1085@gmail.com>
2024-12-17 22:01:06 +0530

    Added Support for Negative Rectangles

--- Geometry.py
+++ Geometry.py
@@ -4,8 +4,8 @@ from random import randint
 class Rectangle:
 
     def __init__(self):
-        self.c1 = randint(0, 99), randint(0, 99)
-        self.c2 = randint(self.c1[0], 100), randint(self.c1[1], 100)
+        self.c1 = randint(0, 100), randint(0, 100)
+        self.c2 = randint(0, 100), randint(0, 100)
         self.area = int(abs(self.c1[0] - self.c2[0]) * abs(self.c1[1] - self.c2[1]))
         print(f"Corners of the Rectangle are {self.c1} and {self.c2}")
 
@@ -25,7 +25,11 @@ class Point:
         self.y = b
 
     def is_in_rectangle(self):
-        if obj1.c1[0] <= self.x <= obj1.c2[0] and obj1.c1[1] <= self.y <= obj1.c2[1]:
+        min_x = min(obj1.c1[0], obj1.c2[0])
+        max_x = max(obj1.c1[0], obj1.c2[0]) + 1
+        min_y = min(obj1.c1[1], obj1.c2[1])
+        max_y = max(obj1.c1[1], obj1.c2[1]) + 1
+        if self.x in range(min_x, max_x) and self.y in range(min_y, max_y):
             print("Point is within the Rectangle")
         else:
             print("Point is outside the Rectangle")
