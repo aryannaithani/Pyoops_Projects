3994611ca2a13232593b0b67c2fef99d3651eb5d
aryannaithani <aryannaithani1085@gmail.com>
2024-12-17 21:08:07 +0530

    Implemented all Graphics

--- Geometry.py
+++ Geometry.py
@@ -21,8 +21,8 @@ class Point:
 class Rectangle:
 
     def __init__(self):
-        self.c1 = randint(0, 10), randint(0, 10)
-        self.c2 = randint(0, 10), randint(0, 10)
+        self.c1 = randint(0, 100), randint(0, 100)
+        self.c2 = randint(0, 100), randint(0, 100)
         self.area = int(abs(self.c1[0] - self.c2[0]) * abs(self.c1[1] - self.c2[1]))
         print(f"Corners of the Rectangle are {self.c1} and {self.c2}")
 
@@ -33,17 +33,26 @@ class Rectangle:
             print(False)
             print(f"You were off by {abs(self.area - z)}")
 
-class Graphics():
+
+class Graphics:
 
     def __init__(self):
         self.my_turtle = turtle.Turtle()
 
     def plot_rectangle(self):
-        pass
+        self.my_turtle.penup()
+        self.my_turtle.goto(obj1.c1[0], obj1.c1[1])
+        self.my_turtle.pendown()
+        self.my_turtle.goto(obj1.c1[0], obj1.c2[1])
+        self.my_turtle.goto(obj1.c2[0], obj1.c2[1])
+        self.my_turtle.goto(obj1.c2[0], obj1.c1[1])
+        self.my_turtle.goto(obj1.c1[0], obj1.c1[1])
 
     def plot_point(self):
+        self.my_turtle.color()
         self.my_turtle.penup()
         self.my_turtle.goto(x, y)
+
 
 obj1 = Rectangle()
 x = int(input("Guess x Co-ords: "))
@@ -52,3 +61,7 @@ z = int(input("Guess Area: "))
 obj2 = Point(x, y)
 obj2.is_in_rectangle()
 obj1.area_is_correct()
+obj3 = Graphics()
+obj3.plot_rectangle()
+obj3.plot_point()
+turtle.done()
