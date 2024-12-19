6abaa500fe9d6d3e35e19260ed09dfaea2b04df5
aryannaithani <aryannaithani1085@gmail.com>
2024-12-20 01:13:48 +0530

    Completed and Organised Project 3

--- MathPainter/generator.py
+++ MathPainter/generator.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, Canvas
+from PIL import Image
+import numpy as np
 
 
 class CanvasGen:
@@ -6,21 +7,24 @@ class CanvasGen:
     def __init__(self, height, width, color):
         self.height = height
         self.width = width
-        self.color = color
-        window = Tk()
-        self.canvas = Canvas(window, height=height, width=width, bg=color)
+        self.data = np.zeros((height, width, 3), dtype=np.uint8)
+        self.data[:] = color
 
     def rectangle(self):
         x1 = int(input("Enter x of rectangle: "))
         y1 = int(input("Enter y of rectangle: "))
-        y2 = int(input("Enter height of rectangle: ")) + y1
-        x2 = int(input("Enter width of rectangle: ")) + x1
-        color = input("Enter color of rectangle: ")
-        self.canvas.create_rectangle(x1,y1,x2,y2,fill=color)
+        height = int(input("Enter height of rectangle: ")) + y1
+        width = int(input("Enter width of rectangle: ")) + x1
+        color = tuple(map(int, input("Enter RGB values for color of rectangle: ").split()))
+        self.data[x1:height, y1:width] = color
 
     def square(self):
         x1 = int(input("Enter x of square: "))
         y1 = int(input("Enter y of square: "))
         side = int(input("Enter side of square: "))
-        color = input("Enter color of square: ")
-        self.canvas.create_rectangle(x1, y1, x1+side, y1+side, fill=color)
+        color = tuple(map(int, input("Enter RGB values for color of square: ").split()))
+        self.data[x1:x1+side, y1:y1+side] = color
+
+    def save_as_png(self, name):
+        img = Image.fromarray(self.data, 'RGB')
+        img.save(name)
--- MathPainter/main.py
+++ MathPainter/main.py
@@ -2,7 +2,7 @@ from MathPainter.generator import CanvasGen
 
 canvas_height = int(input("Enter height of canvas: "))
 canvas_width = int(input("Enter width of canvas: "))
-canvas_color = input("Enter background color of canvas: ")
+canvas_color = tuple(map(int, input("Enter RGB values for background color of canvas: ").split()))
 
 obj1 = CanvasGen(canvas_height, canvas_width, canvas_color)
 
@@ -13,6 +13,5 @@ while True:
     elif command == 'rectangle':
         obj1.rectangle()
     else:
-        obj1.canvas.pack()
-        obj1.canvas.mainloop()
+        obj1.save_as_png('output.png')
         break
