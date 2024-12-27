a3517f714ecd8d374724fe48927424f7adea728d
aryannaithani <aryannaithani1085@gmail.com>
2024-12-28 00:52:34 +0530

    Added form validations

--- CalorieCounter/main.py
+++ CalorieCounter/main.py
@@ -1,5 +1,5 @@
 from flask.views import MethodView
-from wtforms import Form, StringField, SubmitField
+from wtforms import Form, StringField, SubmitField, validators
 from flask import Flask
 from flask import render_template, request
 
@@ -15,16 +15,33 @@ class CalorieFormPage(MethodView):
 
     def get(self):
         calorie_form = CalorieForm()
-        return render_template('calorie-form.html', calorie_form=calorie_form)
+        return render_template('calorie-form.html',
+                               calorie_form=calorie_form)
+
+    def post(self):
+        calorie_form = CalorieForm(request.form)
+        weight = int(calorie_form.weight.data)
+        height = int(calorie_form.height.data)
+        age = int(calorie_form.age.data)
+        country = calorie_form.country.data
+        city = calorie_form.city.data
+        return render_template('calorie-form.html',
+                               calorie_form=calorie_form,
+                               weight=weight,
+                               height=height,
+                               age=age,
+                               country=country,
+                               city=city,
+                               result=True)
 
 
 class CalorieForm(Form):
-    weight = StringField('Weight (in kgs)')
-    age = StringField('Age')
-    height = StringField('Height (in cms)')
-    country = StringField('Country')
-    city = StringField('City')
-    button = SubmitField("Calculate")
+    weight = StringField('Weight (in kgs)',[validators.DataRequired()])
+    age = StringField('Age',[validators.DataRequired()])
+    height = StringField('Height (in cms)',[validators.DataRequired()])
+    country = StringField('Country',[validators.DataRequired()])
+    city = StringField('City',[validators.DataRequired()])
+    button = SubmitField("Calculate",[validators.DataRequired()])
 
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
