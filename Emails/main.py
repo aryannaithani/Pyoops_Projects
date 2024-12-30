3d79473265f8fd455d4f93713da9205c2ea15273
aryannaithani <aryannaithani1085@gmail.com>
2024-12-30 11:27:49 +0530

    Refactoring

--- Emails/main.py
+++ Emails/main.py
@@ -21,12 +21,20 @@ class Articles:
         return out
 
 
-mail = yagmail.SMTP(user='aryannaithani1085@gmail.com',
-                    password='enaexcwqwfkpizao')
-df = pandas.read_excel('people.xlsx')
-for index, row in df.iterrows():
-    subject = f'Hello {row['name']}, Here is your news regarding {row['interest']}'
-    x = Articles(row['interest'], row['name'])
-    mail.send(to=row['email'],
-              contents=x.get(),
-              subject=subject)
+class Email:
+    def __init__(self):
+        self.mail = yagmail.SMTP(user='aryannaithani1085@gmail.com',
+                                 password='enaexcwqwfkpizao')
+        self.data_frame = pandas.read_excel('people.xlsx')
+
+    def send(self):
+        for index, row in self.data_frame.iterrows():
+            subject = f'Hello {row['name']}, Here is your news regarding {row['interest']}'
+            x = Articles(row['interest'], row['name'])
+            self.mail.send(to=row['email'],
+                           contents=x.get(),
+                           subject=subject)
+
+
+sender = Email()
+sender.send()
